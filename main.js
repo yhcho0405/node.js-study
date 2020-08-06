a2150ac62f256f56868f32445799a67cc4fb2634
yhcho0405 <yhcho0405@kakao.com>
2020-08-06 13:02:56 +0900

    add readdir

--- main.js
+++ main.js
@@ -22,29 +22,39 @@ var app = http.createServer(function(request, response) {
     fs.readFile(`data/${queryData.id}`, 'UTF8', function(err, description) {
       if (queryData.id === undefined) {
         title = 'Welcome';
-        description = 'Hello, Hode.js';
+        description = 'Hello, Node.js';
       }
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.writeHead(200);
-      response.end(template);
+      // filesystem
+      // ./data 파일 목록을 filelist에 배열로 저장
+      fs.readdir('./data', function(error, filelist) {
+        var list = `<ul>
+        `;
+        var i = 0;
+        while (i < filelist.length) {
+          list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>
+          `;
+          i++;
+        }
+        list += `</ul>`;
+
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      });
     });
   } else {
     response.writeHead(200);

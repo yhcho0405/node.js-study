fc7e45b41e99c51bab34167ce55784b8119200d7
yhcho0405 <yhcho0405@kakao.com>
2020-08-06 15:54:00 +0900

    add update

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ var app = http.createServer(function(request, response) {
       response.writeHead(200);
       response.end(template);
     });
-  } else if (pathname === '/create_process') { // get POST
+  } else if (pathname === '/create_process') { // get POST create
     var body = '';
     request.on('data', function(data) {
       body += data;
@@ -102,6 +102,48 @@ var app = http.createServer(function(request, response) {
       fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
         response.writeHead(302, {Location: `/?id=${title}`});
         response.end();
+      });
+    });
+  } else if (pathname === '/update') { // update
+    fs.readdir('./data', function(error, filelist) {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+          var title = queryData.id;
+          var list = getTemplateList(filelist);
+          var template = getTemplateHTML(title, list, `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+  } else if (pathname === '/update_process') { // get POST update
+    var body = '';
+    request.on('data', function(data) {
+      body += data;
+      // over 1mb
+      if (body.length > 1e6)
+        request.connection.destroy();
+    });
+    request.on('end', function() {
+      var post = qs.parse(body);
+      var id = post.id
+      var title = post.title;
+      var description = post.description;
+      // 파일 생성
+      fs.rename(`data/${id}`, `data/${title}`, function(error) {
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end();
+        });
       });
     });
   } else { // 404

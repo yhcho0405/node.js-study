709d290caeef46c873f7d6ea2544a2730f29940f
yhcho0405 <yhcho0405@kakao.com>
2020-08-06 15:24:19 +0900

    add creat

--- main.js
+++ main.js
@@ -5,8 +5,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
-function getTemplateHTML(title, list, description) {
+var qs = require('querystring')
+//HTML
+function getTemplateHTML(title, list, description, control) {
   return `
   <!doctype html>
   <html>
@@ -15,28 +16,27 @@ function getTemplateHTML(title, list, description) {
     <meta charset="utf-8">
   </head>
   <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    <h2>${title}</h2>
-    <p>${description}</p>
+    <h1><a href="/">WEB</a></h1>${list}${control}
+    ${description}
   </body>
   </html>
   `;
 }
-
+// 파일 list를 받아 html로 변환 후 스트링 리턴
 function getTemplateList(filelist) {
-  var list = `<ul>
-  `;
+  var list = `
+  <ul>`;
   var i = 0;
   while (i < filelist.length) {
-    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>
-    `;
+    list += `
+    <li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
     i++;
   }
-  list += `</ul>`;
+  list += `
+  </ul>
+  `;
   return list;
 }
-
 // "app" 인스턴스 생성 (객체는 정의만 된 것, 인스턴스는 실체화 된것(메모리 차지))
 // 리스너 형태로 작동
 var app = http.createServer(function(request, response) {
@@ -46,27 +46,68 @@ var app = http.createServer(function(request, response) {
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
   var title = queryData.id;
-
   if (pathname === '/') {
     // file system
     // `data/${queryData.id}`의 텍스트를 description에 담음.
     fs.readFile(`data/${queryData.id}`, 'UTF8', function(err, description) {
+      var control = `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`;
       if (queryData.id === undefined) {
         title = 'Welcome';
         description = 'Hello, Node.js';
+        control = '<a href="/create">create</a>';
       }
       // filesystem
       // ./data 파일 목록을 filelist에 배열로 저장
       fs.readdir('./data', function(error, filelist) {
         var list = getTemplateList(filelist);
-        var template = getTemplateHTML(title, list, description);
+        var template = getTemplateHTML(title, list, `
+          <h2>${title}</h2>
+          <p>${description}</p>
+        `, control);
         response.writeHead(200);
         response.end(template);
       });
     });
-  } else {
-    response.writeHead(200);
+  } else if (pathname === '/create') { // 글쓰기 페이지
+    fs.readdir('./data', function(error, filelist) {
+      var title = 'WEB - create';
+      var list = getTemplateList(filelist);
+      var template = getTemplateHTML(title, list, `
+        <form action="http://localhost:3000/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+      `, '');
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (pathname === '/create_process') { // get POST
+    var body = '';
+    request.on('data', function(data) {
+      body += data;
+      // over 1mb
+      if (body.length > 1e6)
+        request.connection.destroy();
+    });
+    request.on('end', function() {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      // 파일 생성
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+      });
+    });
+  } else { // 404
+    response.writeHead(404);
     response.end('Not found');
   }
 });
+// port 3000
 app.listen(3000);

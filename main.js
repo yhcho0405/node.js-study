427019a2a6371e21ff498a9c8af87da8b458902f
yhcho0405 <yhcho0405@kakao.com>
2020-08-06 09:13:47 +0900

    add comment

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
+// require은 node.js의 모듈 로딩 시스템
+// 외부 모듈을 가져올 수 있음.
+// require('./경로');
+// ./가 없으면 NodeJS 자체 라이브러리에서 찾음.
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
+// app이라는 인스턴스 생성 (객체는 정의만 된 것, 인스턴스는 실체화 된것(메모리 차지))
+// 리스너 형태로 작동
+var app = http.createServer(function(request, response) {
+  // console.log(request.url);
+  var _url = request.url;
+  // console.log(url.parse(_url, true));
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  var title = queryData.id;
+
+  if (pathname === '/') {
+    // file system
+    // `data/${queryData.id}`의 텍스트를 description에 담음.
     fs.readFile(`data/${queryData.id}`, 'UTF8', function(err, description) {
+      if (queryData.id === undefined) {
+        title = 'Welcome';
+        description = 'Hello, Hode.js';
+      }
       var template = `
       <!doctype html>
       <html>
@@ -33,7 +43,12 @@ var app = http.createServer(function(request,response){
       </body>
       </html>
       `;
+      response.writeHead(200);
       response.end(template);
     });
+  } else {
+    response.writeHead(200);
+    response.end('Not found');
+  }
 });
 app.listen(3000);
